34bb619a5e40e00d153d68db41e2b70ef50eb8eb
Botta <venkata.t.botta@accenture.com>
2025-07-02 11:37:54 +0900

    Add or update CalculatorService.js

---
+++ CalculatorService.js
@@ -0,0 +1,11 @@
+class CalculatorService {
+    static add(x, y) {
+        return x + y;
+    }
+
+    static subtract(x, y) {
+        return x - y;
+    }
+}
+
+module.exports = CalculatorService;
