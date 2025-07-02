4bd9f5a47962d1816e85df9ecabaaa69a5d8d573
Botta <venkata.t.botta@accenture.com>
2025-07-02 11:49:06 +0900

    Add or update CalculatorService.js

--- CalculatorService.js
+++ CalculatorService.js
@@ -1,11 +1,65 @@
 class CalculatorService {
-    static add(x, y) {
-        return x + y;
+    constructor() {
+        this.memory = {};
     }
 
-    static subtract(x, y) {
-        return x - y;
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        try {
+            if (b === 0) throw new Error('Cannot divide by zero');
+            return a / b;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    power(base, exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    saveToMemory(key, value) {
+        this.memory[key] = value;
+    }
+
+    recallFromMemory(key) {
+        if (!this.memory.hasOwnProperty(key)) throw new Error('Key not found in memory');
+        return this.memory[key];
+    }
+
+    clearMemory() {
+        this.memory = {};
+    }
+
+    printOperations() {
+        console.log('Available operations:');
+        console.log('1. add(a, b)');
+        console.log('2. subtract(a, b)');
+        console.log('3. multiply(a, b)');
+        console.log('4. divide(a, b)');
+        console.log('5. power(base, exponent)');
+        console.log('6. saveToMemory(key, value)');
+        console.log('7. recallFromMemory(key)');
+        console.log('8. clearMemory()');
     }
 }
 
-module.exports = CalculatorService;
+const calc = new CalculatorService();
+
+const result1 = calc.add(10, 5);
+const result2 = calc.divide(10, 2);
+calc.saveToMemory('lastResult', result2);
+
+console.log('Addition: ' + result1);
+console.log('Division: ' + result2);
+console.log('Memory Value: ' + calc.recallFromMemory('lastResult'));
